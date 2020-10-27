0fae77e12352e97792f921e179f4dae40cf43175
Kris <kris@Lauras-MacBook-Pro-2.local>
2020-10-27 17:06:07 -0500

    [Feature] Add Signup Form to Landing Page

--- server/index.js
+++ server/index.js
@@ -10,6 +10,23 @@ const app = express();
 app.use(express.static(path.join(__dirname, '../build')));
 app.use(bodyParser.json());
 
+app.get('/register', (req, res) => {
+  res.send('success');
+});
+
+app.get('/login', (req, res) => {
+  res.send('success');
+});
+
+app.get('/user', (req, res) => {
+  res.send(req.user);
+});
+
+app.get('/logout', (req, res) => {
+  req.logout();
+  res.send('You have successfully logged out.')
+})
+
 app.listen(PORT, () => {
   console.log(`Listening on port :${PORT}!`);
 });
